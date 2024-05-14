e991287f52d8a1679da4bedf607eb868204f8d6c
matheus-oliveira-andrade <mathnhais@outlook.com>
2024-05-14 20:41:14 -0300

    fix: not creating ingress nginx

--- k8s/create-secrets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl create secret generic rabbit-mq-password --from-literal RabbitMq_Password='123Abc!@#'
-
-kubectl create secret generic movements-db-password --from-literal MovementsDb_Password='123Abc!@#'
--- run.sh
+++ run.sh
@@ -5,6 +5,15 @@ if [[ "$@" =~ "--build-push" ]]; then
   echo "Running build and push of docker images"
   ./build-push.sh    
 fi
+
+echo "Installing ingress nginx"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
+
+echo "Waiting for the ingress controller pod to be up, running, and ready"
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=120s
   
 echo "Creating secrets"
 ./k8s/create-secrets.sh
